725acb4d05d8c87abadd5e0fd42ea72e2ae679fe
Solon <Solonerus@gmail.com>
2023-01-26 15:56:58 +0000

    Server now routes correctly

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response, NextFunction } from "express";
+import axios from "axios";
+import cors from 'cors';
+
+const app = express();
+const port = 3100;
+
+app.listen(port, () => {
+  console.log(`Backend server is alive on port: ${port}.`);
+});
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}))
+
+const vaderSI = (request: Request, response: Response) => {
+  axios
+    .get(
+      "http://localhost:3200/sentimentIntensity?" +
+        request.originalUrl.split("%20").join(" ")
+    )
+    .then((vader_response) => {
+      response.status(200).send(vader_response.data);
+    });
+};
+
+app.get("/vader", vaderSI);
