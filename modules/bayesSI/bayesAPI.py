4432812fead358ba64030cd09c63377e02ad950c
Solon <Solonerus@gmail.com>
2023-03-14 17:19:59 +0000

    Pushes working bayes classifier and API

--- modules/bayesSI/bayesAPI.py
+++ modules/bayesSI/bayesAPI.py
@@ -1,4 +1,4 @@
-import nltk
+from components.bayes import classify, accuracy
 from flask import Flask, request, jsonify
 
 
@@ -8,10 +8,14 @@ app = Flask(__name__)
 def heartbeat():
     return "I'm alive!"
 
-@app.get("/sentimentIntensity/")
-def sentimentIntensity():
+@app.get("/datasetAccuracy/")
+def datasetAccuracy():
+    return jsonify(round(accuracy(), 2))
 
-    return 1
+@app.get("/sentimentPolarity/")
+def sentimentIntensity():
+    userInput = request.args.to_dict()['']
+    return jsonify(classify(userInput))
 
 if __name__ == "__main__":
     app.run(debug=True)
